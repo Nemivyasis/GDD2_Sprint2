64dc9d9b452da1326ece2e61208d9e3e3a7c9ca4
Jonathan So <jds7523@rit.edu>
2018-10-22 20:20:50 -0400

    Added functionality for the "Menu Select" sound and found/fixed a glitch on How to Play.
    - Menu Select sound now triggers; I modified the LevelManager to have
    this work.
    - Added and hooked up a LevelManager to the How To Play scene to avoid
    error.

--- Light The Game/Assets/Scripts/LevelManager.cs
+++ Light The Game/Assets/Scripts/LevelManager.cs
@@ -5,9 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
+	private AudioSource audi; // For playing the menu selection sound effect.
+
+	// Get an instance of the audio source.
+	private void Awake() {
+		audi = GetComponent<AudioSource>();
+	}
+
     //for calling scenes from buttons
     public void LoadLevel(int sceneIndex)
     {
-        SceneManager.LoadScene(sceneIndex);
+		StartCoroutine(WaitAndLoadLevel(sceneIndex));
     }
+
+	/** Plays the "Menu Selection" soundclip, waits until it's finished, then load the level.
+	 * param[sceneIndex] - the index of the scene in BuildSettings that we want to load.
+	 */
+	private IEnumerator WaitAndLoadLevel(int sceneIndex) {
+		if (audi != null && !audi.isPlaying) {
+			audi.Play();
+		}
+		yield return new WaitForSeconds(audi.clip.length);
+		SceneManager.LoadScene(sceneIndex);
+	}
 }
